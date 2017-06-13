39d4c0fbe5c7e3bd7b19f003992b8429e31d130b
Jmz <jamesowers@gmail.com>
2017-06-13 17:50:00 +0100

    Add app wrapper

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,63 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
+    <div id="top-bar" class="row">
+      <div class="col-sm-12">
+        <div class="row">
+          <div class="col-md-4 menu">
+            <router-link to="/">Home</router-link> | <a href="#">Men</a> | <a href="#">Women</a>
+          </div>
+          <div class="col-md-4 text-center">
+            Logo
+          </div>
+          <div class="col-md-4 text-right">
+            <router-link to="/cart">
+              Cart ({{ cartItems.data.length }} items)
+            </router-link>
+          </div>
+        </div>
+      </div>
+    </div>
     <router-view></router-view>
   </div>
 </template>
 
 <script>
+import MoltinService from './services/moltin.js'
+
 export default {
-  name: 'app'
+  name: 'app',
+  data () {
+    return {
+      cartItems: {
+        data: []
+      }
+    }
+  },
+  beforeMount () {
+    MoltinService.getCart().then((response) => {
+      this.cartItems = response
+    })
+  }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-size: 0.9em;
+  }
+
+  #top-bar {
+    background-color: #2691C7;
+    color: #fff;
+    padding: 15px 25px;
+  }
+
+  #top-bar a {
+    padding: 0 10px;
+    color: #fff;
+  }
+
+  #top-bar .menu a:first-child {
+    padding-left: 0;
+  }
 </style>
