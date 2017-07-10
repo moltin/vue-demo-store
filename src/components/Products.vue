7e6321783019a53b35024e3d46fa04a177edc4f8
Drew Watson <drew@moltin.com>
2017-07-10 13:56:27 +0100

    display products on products page

--- src/components/Products.vue
+++ src/components/Products.vue
@@ -4,15 +4,15 @@
    <section class="products">
         <div class="content">
             <div class="product-list">
-                <a class="product-item" href="product.html" v-for="(product, val) in products">
-                    <div class="product-image" style="background: #d9d9d9;">
+                <a class="product-item" href="product.html" v-for="(product, val) in products.data">
+                    <div class="product-image" v-bind:style="{ background: product.background_colour }">
                         <img :src="getProductImage(product)" alt="Crown - A unique black lamp with six metal legs forming a nest at the top, creating a crown of six lights."/>
                     </div>
                     <div class="overlay">
                         <div class="overlay-background" style="background: #aaaaaa;"></div>
                         <div class="overlay-content">
                             <div class="title">{{ product.name }}</div>
-                            <div class="price">{{ product.price }}</div>
+                            <div class="price">{{ product.price.amount }}</div>
                         </div>
                     </div>
                 </a>
@@ -34,19 +34,26 @@ export default {
   beforeMount () {
     MoltinService.getProducts().then((response) => {
       this.products = response
+      console.log(this.products)
     })
   },
   methods: {
-    getProductImage: (product) => {
-      var placeholder = 'https://placeholdit.imgix.net/~text?txtsize=69&txt=824%C3%971050&w=824&h=1050'
+    getProductImage: function (product) {
+      console.log('get product image called')
+      console.log('-----product------')
+      console.log(product)
+      console.log('------------------')
+      var placeholder = 'https://placeholdit.imgix.net/~text?txtsize=69&txt=824%C3%971050&w=824&h=900'
       try {
-        var fileId = product.relationships.files.data[0].id
+        var fileId = product.relationships.files.data['0'].id
+        console.log('file id ' + fileId)
         var file = this.products.included.main_images.find(function (el) {
           return fileId === el.id
         })
         console.log(file.link.href)
         return file.link.href || placeholder
       } catch (e) {
+        console.log('ERROR!! ' + e)
         return placeholder
       }
     }
