fe1a7c5ce3e252d143886e6d3cd6137d81e7a56f
outrunthewolf <chris@outrunthewolf.com>
2019-06-13 15:33:13 +0100

    Adds Ron Swanson to the checkout form Adds multiple error messages for the form Tidies up the form into something that looks nicer

--- src/components/Checkout.vue
+++ src/components/Checkout.vue
@@ -1,114 +1,118 @@
 <template>
+
     <div class="row">
-      <div class="col-md-8 offset-md-2">
-        <form>
 
-          <p>{{ errors }}</p>
+      <div class="ui message" v-bind:class="{ error: isError }" v-if="errors">
 
-          <!-- Customer -->
-          <div class="row">
-            <div class="col-md-6">
-              <div class="form-group">
-                <label for="name">Name</label>
-                <input type="text" class="form-control" id="name" v-model="checkoutData.customer.name" required>
-              </div>
+        <ul id="example-1">
+          <li v-for="error in errors">
+            <div class="header">
+              {{ error.source }}
             </div>
-            <div class="col-md-6">
-              <div class="form-group">
-                <label for="email">Email</label>
-                <input type="email" class="form-control" id="email" v-model="checkoutData.customer.email" required>
-              </div>
-            </div>
-          </div>
-          <!-- Customer -->
+            {{ error.detail }}
+          </li>
+        </ul>
 
-          <div class="row">
-            <div class="col-md-6">
-              <h5>Billing Address</h5>
-              <hr />
-              <!-- Billing Address -->
-              <div class="form-group">
-                <label for="billing-first-name">First Name</label>
-                <input type="text" class="form-control" id="billing-first-name" v-model="checkoutData.billing_address.first_name" required>
-              </div>
-              <div class="form-group">
-                <label for="billing-last-name">Last Name</label>
-                <input type="text" class="form-control" id="billing-last-name" v-model="checkoutData.billing_address.last_name" required>
-              </div>
-              <div class="form-group">
-                <label for="billing-line-1">Address Line 1</label>
-                <input type="text" class="form-control" id="billing-line-1" v-model="checkoutData.billing_address.line_1" required>
-              </div>
-              <div class="form-group">
-                <label for="billing-line-2">Address Line 2</label>
-                <input type="text" class="form-control" id="billing-line-2" v-model="checkoutData.billing_address.line_2" required>
-              </div>
-              <div class="form-group">
-                <label for="billing-city">City</label>
-                <input type="text" class="form-control" id="billing-city" v-model="checkoutData.billing_address.city" required>
-              </div>
-              <div class="form-group">
-                <label for="billing-county">County</label>
-                <input type="text" class="form-control" id="billing-county" v-model="checkoutData.billing_address.county" required>
-              </div>
-              <div class="form-group">
-                <label for="billing-post-code">Post Code</label>
-                <input type="text" class="form-control" id="billing-post-code" v-model="checkoutData.billing_address.postcode" required>
-              </div>
-              <div class="form-group">
-                <label for="billing-country">Country</label>
-                <input type="text" class="form-control" id="billing-country" v-model="checkoutData.billing_address.country" required>
-              </div>
-              <!-- Billing Address -->
-            </div>
-            <div class="col-md-6">
-              <h5>Shipping Address</h5>
-              <hr/>
-              <!-- Shipping Address -->
-              <div class="form-group">
-                <label for="shipping-first-name">First Name</label>
-                <input type="text" class="form-control" id="shipping-first-name" v-model="checkoutData.shipping_address.first_name" required>
-              </div>
-              <div class="form-group">
-                <label for="shipping-last-name">Last Name</label>
-                <input type="text" class="form-control" id="shipping-last-name" v-model="checkoutData.shipping_address.last_name" required>
-              </div>
-              <div class="form-group">
-                <label for="shipping-line-1">Address Line 1</label>
-                <input type="text" class="form-control" id="shipping-line-1" v-model="checkoutData.shipping_address.line_1" required>
-              </div>
-              <div class="form-group">
-                <label for="shipping-line-2">Address Line 2</label>
-                <input type="text" class="form-control" id="shipping-line-2" v-model="checkoutData.shipping_address.line_2" required>
-              </div>
-              <div class="form-group">
-                <label for="shipping-city">City</label>
-                <input type="text" class="form-control" id="shipping-city" v-model="checkoutData.shipping_address.city" required>
-              </div>
-              <div class="form-group">
-                <label for="shipping-county">County</label>
-                <input type="text" class="form-control" id="shipping-county" v-model="checkoutData.shipping_address.county" required>
-              </div>
-              <div class="form-group">
-                <label for="shipping-post-code">Post Code</label>
-                <input type="text" class="form-control" id="shipping-post-code" v-model="checkoutData.shipping_address.postcode" required>
-              </div>
-              <div class="form-group">
-                <label for="shipping-country">Country</label>
-                <input type="text" class="form-control" id="shipping-country" v-model="checkoutData.shipping_address.country" required>
-              </div>
-              <!-- Shipping Address -->
-            </div>
-          </div>
-
-          <div class="row">
-            <div class="col-md-12 text-center">
-              <button class="btn btn-primary" v-on:click.prevent="checkout()">Checkout!</button>
-            </div>
-          </div>
-
-        </form>
       </div>
+
+      <form class="ui form">
+        <h1 class="ui dividing header">Shipping and Billing Details</h1>
+
+        <div class="field">
+
+          <h4 class="ui dividing header">Customer Details</h4>
+          <div class="two fields">
+            <div class="field">
+              <input type="text"class="form-control" id="name" name="Name" placeholder="Name" v-model="checkoutData.customer.name" required>
+            </div>
+            <div class="field">
+              <input type="text" class="form-control" id="email" name="Email" placeholder="Email" v-model="checkoutData.customer.email" required>
+            </div>
+          </div>
+        </div>
+
+        <div class="field">
+          <h4 class="ui dividing header">Billing Address</h4>
+
+          <div class="two fields">
+            <div class="field">
+              <input type="text"class="form-control" id="billing-first-name" name="billing[first-name]" placeholder="First Name" v-model="checkoutData.billing_address.first_name" required>
+            </div>
+            <div class="field">
+              <input type="text" class="form-control" id="billing-last-name" name="billing[last-name]" placeholder="Last Name" v-model="checkoutData.billing_address.last_name" required>
+            </div>
+          </div>
+
+          <div class="two fields">
+            <div class="eight wide field">
+              <input type="text" class="form-control" id="billing-line-1" name="billing[address-1]" placeholder="Address Line 1" v-model="checkoutData.billing_address.line_1" required>
+            </div>
+            <div class="eight wide field">
+              <input type="text" class="form-control" id="billing-line-2" name="billing[address-2]" placeholder="Address Line 2" v-model="checkoutData.billing_address.line_2" required>
+            </div>
+          </div>
+
+          <div class="two fields">
+            <div class="eight wide field">
+              <input type="text" class="form-control" id="billing-city" name="billing[city]" placeholder="City" v-model="checkoutData.billing_address.city" required>
+            </div>
+            <div class="eight wide field">
+              <input type="text" class="form-control" id="billing-county" name="billing[county]" placeholder="County/State" v-model="checkoutData.billing_address.county" required>
+            </div>
+          </div>
+
+          <div class="two fields">
+            <div class="eight wide field">
+              <input type="text" class="form-control" id="billing-postcode" name="billing[postcode]" placeholder="Postcode" v-model="checkoutData.billing_address.postcode" required>
+            </div>
+            <div class="eight wide field">
+              <input type="text"  class="form-control" id="billing-country" name="billing[country]" placeholder="Country" v-model="checkoutData.billing_address.country" required>
+            </div>
+          </div>
+        </div>
+
+        <div class="field">
+          <h4 class="ui dividing header">Shipping Address</h4>
+
+          <div class="two fields">
+            <div class="field">
+              <input type="text"class="form-control" id="shipping-first-name" name="shipping[first-name]" placeholder="First Name" v-model="checkoutData.shipping_address.first_name" required>
+            </div>
+            <div class="field">
+              <input type="text" class="form-control" id="shipping-last-name" name="shipping[last-name]" placeholder="Last Name" v-model="checkoutData.shipping_address.last_name" required>
+            </div>
+          </div>
+
+          <div class="two fields">
+            <div class="eight wide field">
+              <input type="text" class="form-control" id="shipping-line-1" name="shipping[address-1]" placeholder="Address Line 1" v-model="checkoutData.shipping_address.line_1" required>
+            </div>
+            <div class="eight wide field">
+              <input type="text" class="form-control" id="shipping-line-2" name="shipping[address-2]" placeholder="Address Line 2" v-model="checkoutData.shipping_address.line_2" required>
+            </div>
+          </div>
+
+          <div class="two fields">
+            <div class="eight wide field">
+              <input type="text" class="form-control" id="shipping-city" name="shipping[city]" placeholder="City" v-model="checkoutData.shipping_address.city" required>
+            </div>
+            <div class="eight wide field">
+              <input type="text" class="form-control" id="shipping-county" name="shipping[county]" placeholder="County/State" v-model="checkoutData.shipping_address.county" required>
+            </div>
+          </div>
+
+          <div class="two fields">
+            <div class="eight wide field">
+              <input type="text" class="form-control" id="shipping-postcode" name="shipping[postcode]" placeholder="Postcode" v-model="checkoutData.shipping_address.postcode" required>
+            </div>
+            <div class="eight wide field">
+              <input type="text"  class="form-control" id="shipping-country" name="shipping[country]" placeholder="Country" v-model="checkoutData.shipping_address.country" required>
+            </div>
+          </div>
+        </div>
+
+        <button class="btn btn-primary" v-on:click.prevent="checkout()">Checkout!</button>
+      </form>
     </div>
 </template>
 <script>
@@ -118,15 +122,16 @@ export default {
   name: 'product',
   data () {
     return {
+      isError: false,
       errors: '',
       checkoutData: {
         customer: {
-          name: 'aa',
-          email: 'aa@aa.com'
+          name: 'Ron Swanson',
+          email: 'ron@swanson.com'
         },
         shipping_address: {
-          first_name: 'John',
-          last_name: 'Doe',
+          first_name: 'Ron',
+          last_name: 'Swanson',
           line_1: '2nd Floor British India House',
           line_2: '15 Carliol Square',
           city: 'Newcastle Upon Tyne',
@@ -135,8 +140,8 @@ export default {
           country: 'United Kingdom'
         },
         billing_address: {
-          first_name: 'John',
-          last_name: 'Doe',
+          first_name: 'Ron',
+          last_name: 'Swanson',
           line_1: '2nd Floor British India House',
           line_2: '15 Carliol Square',
           city: 'Newcastle Upon Tyne',
@@ -149,10 +154,11 @@ export default {
   },
   methods: {
     checkout () {
-      MoltinService.checkout(this.checkoutData).then((response) => {
+      MoltinService.checkout(this.checkoutData.customer, this.checkoutData.billing_address, this.checkoutData.shipping_address).then((response) => {
         this.$router.push('/payments/' + response.data.id)
       }).catch((err) => {
-        this.errors = 'There was a problem checking out' + err
+        this.isError = true
+        this.errors = err.errors
       })
     }
   }
